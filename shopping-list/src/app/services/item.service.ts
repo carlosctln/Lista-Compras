f813d213295d17376fac841af2272dcea9ace9f2
Carlos Catalan <catalanc12@gmail.com>
2022-03-01 15:32:19 -0600

    aplicacion angular

---
+++ shopping-list/src/app/services/item.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Item } from 'src/app/models/Item';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+    url: string = 'http://localhost:3000/items';
+    httpOptions = {
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    };
+    items: Item[] = [
+        {
+            id: 0,
+            title: 'manzana',
+            price: 10.5,
+            quantity: 4,
+            completed: false
+          },
+          {
+            id: 1,
+            title: 'pan',
+            price: 3.5,
+            quantity: 8,
+            completed: true
+          },
+          {
+            id: 2,
+            title: 'Camisa',
+            price: 300,
+            quantity: 8,
+            completed: false
+          }
+    ];
+
+  constructor(private http:HttpClient) { }
+
+  getItems():Observable<Item[]>{
+        return this.http.get<Item[]>(this.url)
+  }
+
+  addItem(item: Item):Observable<Item>{
+    return this.http.post<Item>(this.url, item, this.httpOptions);
+  }
+
+  toggleItem(item: Item):Observable<Item>{
+    return this.http.put<Item>(this.url + item.id, item, this.httpOptions);
+  }
+
+  deleteItem(item: Item):Observable<Item>{
+    return this.http.delete<Item>(this.url + item.id);
+  }
+}
